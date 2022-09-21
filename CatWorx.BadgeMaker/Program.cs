cf3f98dc07de3081702f0b1724770e854b188762
Justin Vititoe <jvit1985@yahoo.com>
2022-09-21 14:25:46 -0500

    added ability to create badges for employees

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CatWorx.BadgeMaker
 {
@@ -31,11 +32,12 @@ namespace CatWorx.BadgeMaker
         //This is important!
         return employees;
     }
-    static void Main(string[] args)
+    async static Task Main(string[] args)
     {
       List<Employee> employees = GetEmployees();
       Util.PrintEmployees(employees);
       Util.MakeCSV(employees);
+      await Util.MakeBadges(employees);
     }
   }
 }
